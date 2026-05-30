f9d1b8e4c80ce787246b72d12bef0ca30f05073e
maslina524 <luk.kirill2804@gmail.com>
2026-05-31 00:12:24 +0300

    added request method to Client

--- src/client.rs
+++ src/client.rs
@@ -1,5 +1,8 @@
-use std::sync::Arc;
-use reqwest;
+use std::{collections::HashMap, sync::Arc};
+
+use reqwest::{self, Method, Url};
+use serde_json::Value;
+
 use crate::events::EventsAPI;
 
 #[derive(Clone)]
@@ -18,5 +21,20 @@ impl Client {
 
     pub fn events(&self) -> EventsAPI {
         EventsAPI::new(self.clone())
+    }
+
+    pub(crate) async fn request(&self, url: &str, method: Method, query: HashMap<&str, &str>) -> Result<(Value, u16), Box<dyn std::error::Error>> {
+        let params_url = Url::parse_with_params(url, query)?;
+        let response = self.http.request(method, params_url)
+            .header("Authorization", format!("Bearer {}", self.key))
+            .send()
+            .await?;
+
+        let status_code = response.status().as_u16();
+        let raw_text = response.text().await?;
+
+        let json_value: Value = serde_json::from_str(&raw_text)?;
+
+        Ok((json_value, status_code))
     }
 }
