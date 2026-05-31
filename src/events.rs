9aa8e9cbfcc1cd4d4b16f05919fdb53f5f42c84b
maslina524 <luk.kirill2804@gmail.com>
2026-05-31 15:57:34 +0300

    added rotation method to events.rs

--- src/events.rs
+++ src/events.rs
@@ -1,12 +1,30 @@
 use std::collections::HashMap;
+use chrono::{DateTime, Utc, NaiveDateTime};
 
 use serde_json::{Value, Map};
 use reqwest::Method;
 
-use crate::{Client, Result};
+use crate::{Client, Result, get_object};
 use crate::{get_string, get_i64, get_array};
 
 const GAMEMODES_ENDPOINT: &str = "https://api.brawlstars.com/v1/gamemodes";
+const ROTATION_ENDPOINT: &str = "https://api.brawlstars.com/v1/events/rotation";
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Event {
+    id: u32,
+    map: String,
+    mode_str: String,
+    mode_id: i64
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct EventSlot {
+    start_time: DateTime<Utc>,
+    end_time: DateTime<Utc>,
+    slot_id: u32,
+    event: Event
+}
 
 pub struct EventsAPI {
     inner: Client
@@ -45,4 +63,52 @@ impl EventsAPI {
 
         Ok(ret)
     }
+
+    pub async fn rotation(&self) -> Result<Vec<EventSlot>> {
+        let value = self.inner.request(
+            ROTATION_ENDPOINT, 
+            Method::GET, 
+            HashMap::new()
+        ).await?;
+        // println!("{value:#?}");
+        let mut ret = Vec::new();
+        let vec = value
+            .as_array()
+            .ok_or("Strange Response")?;
+
+        for value in vec {
+            let obj = value
+                .as_object()
+                .ok_or("Strange Response")?;
+
+            let start_time_str = get_string!(&obj, "startTime").ok_or("Strange Response")?;
+            let start_time = date_time_from_str(start_time_str);
+
+            let end_time_str = get_string!(&obj, "endTime").ok_or("Strange Response")?;
+            let end_time = date_time_from_str(end_time_str);
+
+            let slot_id = get_i64!(&obj, "slotId").ok_or("Strange Response")? as u32;
+
+            let event_obj = get_object!(&obj, "event").ok_or("Strange Response")?;
+
+            let id = get_i64!(&event_obj, "id").ok_or("Strange Response")? as u32;
+            let map = get_string!(&event_obj, "map").ok_or("Strange Response")?.to_string();
+            let mode_str = get_string!(&event_obj, "mode").ok_or("Strange Response")?.to_string();
+            let mode_id = get_i64!(&event_obj, "modeId").ok_or("Strange Response")?;
+
+            let event = Event { id, map, mode_str, mode_id };
+            let event_slot = EventSlot { start_time, end_time, slot_id, event };
+
+            ret.push(event_slot);
+        }
+
+        Ok(ret)
+    }
+}
+
+fn date_time_from_str(string: &str) -> DateTime<Utc> {
+    DateTime::<Utc>::from_naive_utc_and_offset(
+        NaiveDateTime::parse_from_str(string, "%Y%m%dT%H%M%S%.3fZ").unwrap(),
+        Utc,
+    )
 }
--- src/lib.rs
+++ src/lib.rs
@@ -19,4 +19,14 @@ mod tests {
         let ret = client.events().gamemodes().await;
         println!("{ret:#?}");
     }
+
+    #[tokio::test]
+    async fn rotation_test() {
+        dotenv().ok();
+
+        let api_key = std::env::var("API_KEY").unwrap_or_default();
+        let client = Client::new(api_key);
+        let ret = client.events().rotation().await;
+        println!("{ret:#?}");
+    }
 }
