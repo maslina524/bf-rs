98137bbc93b36adc023f246e71cbeca518487921
maslina524 <luk.kirill2804@gmail.com>
2026-05-31 03:25:04 +0300

    added CrateError to client.rs

--- src/client.rs
+++ src/client.rs
@@ -3,24 +3,39 @@ use std::{collections::HashMap, sync::Arc};
 use reqwest::{self, Method, Url};
 use serde_json::Value;
 use thiserror::Error;
+use url;
 
 use crate::events::EventsAPI;
 
 #[derive(Debug, PartialEq, Error)]
 pub enum ApiError {
     #[error("Incorrect Params (400): {0}")]
-    IncorrectParams(String), // 400
+    IncorrectParams(String),
     #[error("Access Denied (403): {0}")]
-    AccessDenied(String),    // 403
+    AccessDenied(String),
     #[error("Not Found (404): {0}")]
-    NotFound(String),        // 404
+    NotFound(String),
     #[error("Rate Limit (429): {0}")]
-    RateLimit(String),       // 429
+    RateLimit(String),
     #[error("Unknown (500): {0}")]
-    Unknown(String),         // 500
+    Unknown(String),
     #[error("Unavailable (503): {0}")]
-    Unavailable(String)      // 503
+    Unavailable(String)
 }
+
+#[derive(Debug, Error)]
+pub enum CrateError {
+    #[error("Api Error: {0}")]
+    IncorrectParams(#[from] ApiError),
+    #[error("Json Error: {0}")]
+    SerdeJson(#[from] serde_json::Error),
+    #[error("Http Error: {0}")]
+    Http(#[from] reqwest::Error),
+    #[error("URL parsing error: {0}")]
+    UrlParseError(#[from] url::ParseError),
+}
+
+pub type Result<T> = std::result::Result<T, CrateError>;
 
 #[derive(Clone)]
 pub struct Client {
@@ -40,17 +55,39 @@ impl Client {
         EventsAPI::new(self.clone())
     }
 
-    pub(crate) async fn request(&self, url: &str, method: Method, query: HashMap<&str, &str>) -> Result<Value, Box<dyn std::error::Error>> {
+    pub(crate) async fn request(&self, url: &str, method: Method, query: HashMap<&str, &str>) -> Result<Value> {
         let params_url = Url::parse_with_params(url, query)?;
         let response = self.http.request(method, params_url)
             .header("Authorization", format!("Bearer {}", self.key))
             .send()
             .await?;
 
-        let status_code = response.status().as_u16();
+        let status = response.status();
         let raw_text = response.text().await?;
-        let json_value: Value = serde_json::from_str(&raw_text)?;
 
-        Ok(json_value)
+        if status.is_success() {
+            let json_value = serde_json::from_str::<Value>(&raw_text)?;
+            Ok(json_value)
+        } else {
+            let error_msg = serde_json::from_str::<Value>(&raw_text)?
+                .as_object()
+                .unwrap()
+                .get("message")
+                .unwrap()
+                .as_str()
+                .unwrap()
+                .to_string();
+
+            let api_error = match status.as_u16() {
+                400 => ApiError::IncorrectParams(error_msg),
+                403 => ApiError::AccessDenied(error_msg),
+                404 => ApiError::NotFound(error_msg),
+                429 => ApiError::RateLimit(error_msg),
+                503 => ApiError::Unavailable(error_msg),
+                _ if status.is_server_error() => ApiError::Unknown(error_msg),
+                _ => ApiError::Unknown(format!("Http {}: {}", status, error_msg)),
+            };
+            Err(CrateError::IncorrectParams(api_error))
+        }
     }
 }
