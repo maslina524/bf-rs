f144b07e2c4714ad9217aac67b0c1d883a071e79
maslina524 <luk.kirill2804@gmail.com>
2026-05-31 15:56:46 +0300

    added get_object macro to json.rs

--- src/json.rs
+++ src/json.rs
@@ -27,3 +27,13 @@ macro_rules! get_array {
         _map.get(_key).and_then(|v| v.as_array())
     }};
 }
+
+#[macro_export]
+macro_rules! get_object {
+    ($map:expr, $key:expr) => {{
+        let _map: &Map<String, Value> = $map;
+        let _key: &str = &$key;
+
+        _map.get(_key).and_then(|v| v.as_object())
+    }};
+}
